7bece373277ceb0de0dcee6df5ab934cbe5826d8
tellthemachines <isabel@tellthemachines.com>
2023-09-01 16:30:02 +1000

    Add back group variation controls

--- packages/block-editor/src/components/block-inspector/index.js
+++ packages/block-editor/src/components/block-inspector/index.js
@@ -255,7 +255,7 @@ const BlockInspectorSingleBlock = ( { clientId, blockName } ) => {
 				{ ...blockInformation }
 				className={ blockInformation.isSynced && 'is-synced' }
 			/>
-			{ /* <BlockVariationTransforms blockClientId={ clientId } /> */ }
+			<BlockVariationTransforms blockClientId={ clientId } />
 			<BlockInfo.Slot />
 			{ showTabs && (
 				<InspectorControlsTabs
