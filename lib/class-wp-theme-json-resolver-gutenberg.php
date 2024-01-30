776f363015eab5b627f84429454332083056f213
Alex Lende <alex+github.com@lende.xyz>
2024-01-30 14:51:25 -0600

    Merge class-wp-theme-json-resolver-test tests from core

--- lib/class-wp-theme-json-resolver-gutenberg.php
+++ lib/class-wp-theme-json-resolver-gutenberg.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * WP_Theme_JSON_Resolver class
+ * WP_Theme_JSON_Resolver_Gutenberg class
  *
- * @package gutenberg
+ * @package Gutenberg
  * @since 5.8.0
  */
 
@@ -18,6 +18,7 @@ if ( class_exists( 'WP_Theme_JSON_Resolver_Gutenberg' ) ) {
  * This is a low-level API that may need to do breaking changes. Please,
  * use gutenberg_get_global_settings, gutenberg_get_global_styles, and gutenberg_get_global_stylesheet instead.
  *
+ * @since 5.8.0
  * @access private
  */
 #[AllowDynamicProperties]
